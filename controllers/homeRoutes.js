eeda8a13d46ed4aecde785ac0b96cfa4fb7e7f10
sydnief <sydniefarrell@gmail.com>
2023-07-07 23:56:54 -0400

    updated controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -15,6 +16,30 @@ router.get('/', async (req, res) => {
     // Pass serialized data into Handlebars.js template
     res.render('homepage', { users });
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+// GET one painting
+// Use the custom middleware before allowing the user to access the painting
+router.get('/painting/:id', withAuth, async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
